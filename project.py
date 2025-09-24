894e1063a4e96e2b9e02e3fd352b37747602cc75
Hatem Ayman <hatem.ayman.508@gmail.com>
2025-09-24 11:16:06 +0300

    Project structure

---
+++ project.py
@@ -0,0 +1,21 @@
+import sys, re, argparse
+
+
+def main():
+    ...
+
+
+def convert_line(line: str) -> str:
+    ...
+
+
+def apply_inline_formatting(text: str) -> str:
+    ...
+
+
+def convert_markdown(md_text: str) -> str:
+    ...
+
+
+if __name__ == "__main__":
+    main()
